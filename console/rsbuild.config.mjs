8eb058adb8e5f23610610f656713161bf3ee5741
Ryan Wang <i@ryanc.cc>
2025-06-18 12:42:16 +0800

    chore: replace vite with rsbuild
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ console/rsbuild.config.mjs
@@ -0,0 +1,18 @@
+import { rsbuildConfig } from "@halo-dev/ui-plugin-bundler-kit";
+import Icons from "unplugin-icons/rspack";
+import { UnoCSSRspackPlugin } from "@unocss/webpack/rspack";
+
+export default rsbuildConfig({
+  rsbuild: {
+    tools: {
+      rspack: {
+        plugins: [
+          Icons({
+            compiler: "vue3",
+          }),
+          UnoCSSRspackPlugin(),
+        ],
+      },
+    },
+  },
+});
